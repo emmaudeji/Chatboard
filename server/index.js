fc7491a6d922df52d9ba9a5f71d20394bcc38ce1
Emma Udeji <gogreneinfo@gmail.com>
2023-04-28 13:25:12 +0000

    server running ;

--- server/index.js
+++ server/index.js
@@ -3,3 +3,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '31mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '31mb', extended: true }))
+app.use(cors());
+
+// setup mongodb
+
+const CONNECTION_URL = 'mongodb+srv://gogreneinfo:cat89boy@cluster0.pwt69y1.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set();
