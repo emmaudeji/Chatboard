68ed20ce208884c17bd7ae875ccedb70d46d9143
Emma Udeji <gogreneinfo@gmail.com>
2023-04-29 17:39:43 +0000

    implelmented CRUD functionality

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
-
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '31mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '31mb', extended: true }))
@@ -16,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes)
 
 // setup mongodb
-const CONNECTION_URL = 'mongodb+srv://gogreneinfo:cat89boy@cluster0.pwt69y1.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
